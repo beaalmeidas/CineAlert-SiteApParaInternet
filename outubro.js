5a5bfd59db3458e8286e13fcd6cacc66e3d9e6fe
João Victor <129428389+odulosxp9@users.noreply.github.com>
2024-05-26 08:43:35 -0300

    Update outubro.js

--- outubro.js
+++ outubro.js
@@ -1,34 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const OctSem1 = [
-        { title: "Halloween Ends", text: "Direção: David Gordon Green" },
-        { title: "Black Panther: Wakanda Forever", text: "Direção: Ryan Coogler" },
-        { title: "Aquaman and the Lost Kingdom", text: "Direção: James Wan" },
-        { title: "Thor: Love and Thunder", text: "Direção: Taika Waititi" },
-        { title: "Spider-Man: Across the Spider-Verse (Part One)", text: "Direção: Joaquim Dos Santos, Kemp Powers, Justin K. Thompson" },
+        { title: "Joker: Folie à Deux", text: "Direção: Todd Phillips" },
+        { title: "La nouvelle femme", text: "Direção: Léa Todorov" },
+        { title: "Nina et le secret du Hérrisson", text: "Direção: Alain Gagnol, Jean-Loup Felicioli" },
     ];
 
     const OctSem2 = [
-        { title: "Doctor Strange in the Multiverse of Madness", text: "Direção: Sam Raimi" },
-        { title: "Black Widow", text: "Direção: Cate Shortland" },
-        { title: "Eternals", text: "Direção: Chloé Zhao" },
-        { title: "Shang-Chi and the Legend of the Ten Rings", text: "Direção: Destin Daniel Cretton" },
-        { title: "Blade", text: "Direção: Bassam Tariq" },
+        { title: "Arthur's Whisky", text: "Direção: Stephen Cookson" },
+        { title: "Chief of Station", text: "Direção: Jesse V. Johnson" },
+        { title: "Irena's Vow", text: "Direção: Louise Archambault" },
+        { title: "Robot Selvagem", text: "Direção: Chris Sanders" },
     ];
 
     const OctSem3 = [
-        { title: "Mission: Impossible 8", text: "Direção: Christopher McQuarrie" },
-        { title: "Fantastic Beasts: The Secrets of Dumbledore", text: "Direção: David Yates" },
-        { title: "The Flash", text: "Direção: Andy Muschietti" },
-        { title: "John Wick: Chapter 5", text: "Direção: Chad Stahelski" },
-        { title: "Avatar 3", text: "Direção: James Cameron" },
+        { title: "Little Emma", text: "Elenco e detalhes ainda não disponíveis" },
+        { title: "O Jogo da Rainha", text: "Elenco e detalhes ainda não disponíveis" },
     ];
 
     const OctSem4 = [
-        { title: "The Marvels", text: "Direção: Nia DaCosta" },
-        { title: "Spider-Man: Beyond the Spider-Verse (Part Two)", text: "Direção: Joaquim Dos Santos, Kemp Powers, Justin K. Thompson" },
-        { title: "Guardians of the Galaxy Vol. 4", text: "Direção: James Gunn" },
-        { title: "A Quiet Place Part III", text: "Direção: Jeff Nichols" },
-        { title: "Joker: Folie à Deux", text: "Direção: Todd Phillips" },
+        { title: "Ainda não foram confirmados lançamentos específicos para esta semana.", text: "" },
     ];
 
     function createCard(movie) {
